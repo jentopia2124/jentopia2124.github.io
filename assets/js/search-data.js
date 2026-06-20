1f368af2a78afb3304a2282814ce62e1a83c6c7a
jentopia2124 <121466705+jentopia2124@users.noreply.github.com>
2026-06-20 01:42:13 +0000

    Deploying to gh-pages from @ jentopia2124/jentopia2124.github.io@01a309033bdbd9202710967e584b2e7d3623258e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "List of my cool repos",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -50,13 +50,6 @@ ninja.data = [{
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/blog/";
-              },
-            },{id: "dropdown-poker",
-              title: "poker",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
               },
             },{id: "post-a-post-with-plotly-js",
         
@@ -442,46 +435,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -494,21 +447,21 @@ ninja.data = [{
         title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/msp-math", "_blank");
+          window.open("https://github.com/jentopia2124", "_blank");
         },
       },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/Minseong Park", "_blank");
+          window.open("https://www.linkedin.com/in/minseongpark", "_blank");
         },
       },{
         id: 'social-medium',
         title: 'Medium',
         section: 'Socials',
         handler: () => {
-          window.open("https://medium.com/@Minseong Park", "_blank");
+          window.open("https://medium.com/@jentopia", "_blank");
         },
       },{
         id: 'social-custom_social',
